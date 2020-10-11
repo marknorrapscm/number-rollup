5367f60f3788a3037bad8df3fde807ab82138104
Mark <mark>
2020-10-11 22:37:33 +0100

    feat: prevent two animations running simultaneously on the same target element

--- src/get-targets.js
+++ src/get-targets.js
@@ -3,42 +3,41 @@ export default (userOptions) => {
 
 	if (userOptions && userOptions.id) {
 		const newTarget = generateSingleTargetFromOptions(userOptions);
-		targetElements.push(newTarget);
+		if (newTarget) {
+			targetElements.push(newTarget);
+		}
 	} else {
 		const targets = readTargetsSpecifiedInDom(userOptions);
 		targetElements = targets;
 	}
 
+	if (targetElements.length === 0) {
+		console.warn("number-rollup animation was triggered but no target elements were found");
+	}
+
 	return targetElements;
 };
 
-const determineDirection = (startNumber, endNumber) => {
-	if (startNumber < endNumber) {
-		return Direction.Ascending;
-	} else {
-		return Direction.Descending;
-	}
-};
-
-const Direction = Object.freeze({
-	Ascending: "ascending",
-	Descending: "descending",
-});
-
 const generateSingleTargetFromOptions = (userOptions) => {
 	const domElement = document.getElementById(userOptions.id);
-	const range = userOptions.endNumber - userOptions.startNumber;
-	const incrementPerMillisecond = range / userOptions.duration;
-	const direction = determineDirection(userOptions.startNumber, userOptions.endNumber);
 
-	return {
-		domElement,
-		startNumber: userOptions.startNumber,
-		endNumber: userOptions.endNumber,
-		incrementPerMillisecond,
-		formatNumber: userOptions.formatNumber,
-		direction,
-	};
+	if (!isDomElementCurrentBeingAnimated(domElement)) {
+		const startNumber = Number(userOptions.startNumber);
+		const endNumber = Number(userOptions.endNumber);
+		const duration = Number(userOptions.duration);
+		const range = endNumber - startNumber;
+		const incrementPerMillisecond = range / duration;
+		const direction = determineDirection(startNumber, endNumber);
+
+		return {
+			domElement,
+			startNumber,
+			endNumber,
+			incrementPerMillisecond,
+			formatNumber: userOptions.formatNumber,
+			direction,
+		};
+	}
 };
 
 const readTargetsSpecifiedInDom = (userOptions) => {
@@ -46,8 +45,10 @@ const readTargetsSpecifiedInDom = (userOptions) => {
 	const targetElements = [];
 
 	domElements.forEach((domElement) => {
-		const newTarget = generateTargetFromDomElement(domElement, userOptions);
-		targetElements.push(newTarget);
+		if (!isDomElementCurrentBeingAnimated(domElement)) {
+			const newTarget = generateTargetFromDomElement(domElement, userOptions);
+			targetElements.push(newTarget);
+		}
 	});
 
 	return targetElements;
@@ -79,4 +80,21 @@ const readOptionsFromDomElement = (domElement) => {
 		endNumber,
 		duration,
 	};
+};
+
+const determineDirection = (startNumber, endNumber) => {
+	if (startNumber < endNumber) {
+		return Direction.Ascending;
+	} else {
+		return Direction.Descending;
+	}
+};
+
+const Direction = Object.freeze({
+	Ascending: "ascending",
+	Descending: "descending",
+});
+
+const isDomElementCurrentBeingAnimated = (domElement) => {
+	return domElement.classList.contains("number-rollup-is-active");
 };
